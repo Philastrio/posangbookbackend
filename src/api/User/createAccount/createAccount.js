2d5086aca744d17ebd00e45a3c65acca3b838221
HanKang <han_gang@naver.com>
2019-07-02 07:34:41 +0900

    createAccount 수정

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -4,14 +4,28 @@ export default {
   Mutation: {
     createAccount: async (_, args) => {
       const { userName, email, position, belongTo, Rank = "" } = args;
-      const user = await prisma.createUser({
+      const exists = await prisma.$exists.user({
+        OR: [
+          {
+            userName
+          },
+          {
+            email
+          }
+        ]
+      });
+
+      if (exists) {
+        throw Error("입력하신 이름 또는 이메일은 이미 존재합니다");
+      }
+      await prisma.createUser({
         userName,
         email,
         position,
         belongTo,
         Rank
       });
-      return user;
+      return true;
     }
   }
 };
