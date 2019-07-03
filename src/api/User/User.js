741dd552cf58d220734264b9657d7e4004fb74b7
HanKang <han_gang@naver.com>
2019-07-04 06:34:30 +0900

    본인확인 과정 만드는 중

--- src/api/User/User.js
+++ src/api/User/User.js
@@ -2,6 +2,11 @@ import { prisma } from "../../../generated/prisma-client";
 
 export default {
   User: {
-    posts: ({ id }) => prisma.user({ id }).posts()
+    posts: ({ id }) => prisma.user({ id }).posts(),
+    isSelf: (parent, _, { request }) => {
+      const { user } = request;
+      const { id: parentId } = parent; // parent에서 id를 가져오는데 그걸 parentId라고 한다.
+      return user.id === parentId; // 가져온 parentId랑 request의 id랑 같은지 boolean
+    }
   }
 };
