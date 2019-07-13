49c320b592b11dd956219a6c4d719a851440eba6
HanKang <han_gang@naver.com>
2019-07-14 05:59:12 +0900

    작업일지 확인

--- src/api/User/me/me.js
+++ src/api/User/me/me.js
@@ -3,7 +3,7 @@ import { prisma } from "../../../../generated/prisma-client";
 export default {
   Query: {
     me: async (_, __, { request, isAuthenticated }) => {
-      isAuthenticated(request);
+      await isAuthenticated(request);
       const { user } = request;
       return await prisma.user({ id: user.id });
     }
