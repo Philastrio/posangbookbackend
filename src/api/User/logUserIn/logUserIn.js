a6c46e1e697a0eb5292aab863de39ff863926659
philastrio <philastrio@gmail.com>
2019-07-15 20:36:13 +0900

    token?

--- src/api/User/logUserIn/logUserIn.js
+++ src/api/User/logUserIn/logUserIn.js
@@ -1,1 +1,11 @@
-export default {};
+import { prisma } from "../../../../generated/prisma-client";
+
+export default {
+  Mutation: {
+    logUserIn: async (_, args) => {
+      const { token } = args;
+      const user = await prisma.user({ token });
+      return await prisma.user({ token: token });
+    }
+  }
+};
